0d0961e61e49ca94ad621054620cb7a0f4e056d2
Gab-Mel <gabrielzinho.melo.10@gmail.com>
2023-12-08 11:56:20 +0000

    creating tests for progress model

--- tests/model_progress.py
+++ tests/model_progress.py
@@ -40,3 +40,70 @@ def test_create_progress_failure(progress_repository):
 
     assert created_progress is None
     assert progress_repository.db.execute.called_once
+
+def test_read_progress_with_user_and_goal(progress_repository):
+    mock_result = [("123", "123", 1)]
+    progress_repository.db.execute.return_value = mock_result
+
+    progress = progress_repository.read(user="123", goal="123")
+    args, _ = progress_repository.db.execute.call_args
+
+    assert progress is not None
+    assert progress_repository.db.execute.called_once
+    assert args[1] == ("123", "123")
+
+def test_update_progress(progress_repository):
+    mock_result = [("123", "123", 1)]
+    progress_repository.db.execute.return_value = mock_result
+
+    progress_data = {
+        "user": "123",
+        "goal": "123",
+        "page": 0.5,
+    }
+
+    new_progress = progress_repository.update(**progress_data)
+
+    assert new_progress is not None
+    assert progress_repository.db.execute.called_once
+
+def test_update_progress_failure(progress_repository):
+    progress_data = {
+        "user": "845",
+        "goal": "599",
+        "page": "ssdd",
+    }
+
+    progress_repository.db.execute.return_value = None
+
+    updated_progress = progress_repository.update(**progress_data)
+
+    assert updated_progress is None
+    assert progress_repository.db.execute.called_once
+
+def test_delete_progress(progress_repository):
+    mock_result = [("123", "123", 1)]
+    progress_repository.db.execute.return_value = mock_result
+
+    progress_data = {
+        "user": "123",
+        "goal": "123",
+    }
+
+    new_progress = progress_repository.delete(**progress_data)
+
+    assert new_progress is not None
+    assert progress_repository.db.execute.called_once
+
+def test_delete_progress_failure(progress_repository):
+    progress_data = {
+        "user": "845",
+        "goal": "599",
+    }
+
+    progress_repository.db.execute.return_value = None
+
+    deleted_progress = progress_repository.delete(**progress_data)
+
+    assert deleted_progress is None
+    assert progress_repository.db.execute.called_once
